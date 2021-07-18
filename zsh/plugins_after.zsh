473ad040a12ca6d6fabc289cfe6acfef261d4639
vmicic <vukasin.micic5@gmail.com>
2021-07-18 18:21:30 +0200

    Update dotfiles

--- zsh/plugins_after.zsh
+++ zsh/plugins_after.zsh
@@ -4,3 +4,5 @@
 
 source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ~/.zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
