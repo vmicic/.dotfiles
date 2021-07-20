82740f478dda0646af6edd3de7558a70c7914a43
vmicic <vukasin.micic5@gmail.com>
2021-07-20 10:43:07 +0200

    Add git log aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,3 +9,5 @@ alias c='clear'
 alias gs="git status"
 alias gc="git commit"
 alias v="vim"
+alias gl="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias gl2="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
